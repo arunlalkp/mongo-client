a9e444f735560ef1e865c31d09ffb55f39d511a7
Arun Lal <arunlalkp.kp@gmail.com>
2019-03-27 15:48:00 +0530

    mongodb connected via mongoClient

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const morgan = require('morgan')
 const db = require('mongodb')
+const MongoClient = db.MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/mongoClient', {useNewUrlParser: true} , (err, dbase)=> {
+    if(err) console.log(err);
+    else console.log(`Mongodb Connected.. on  ${dbase.s.url}`);    
+} )
+
+
+
+
+
 
 const app = express();
 
